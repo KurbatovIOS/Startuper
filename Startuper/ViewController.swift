59baa36e5ab0de883ab498bce93e03d223e635b1
KurbatovIOS <akurbatov.work@gmail.com>
2023-01-19 21:00:08 +0300

    Initial Commit

---
+++ Startuper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Startuper
+//
+//  Created by Kurbatov Artem on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
